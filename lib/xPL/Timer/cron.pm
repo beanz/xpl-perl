96987c9516f9b72bbed569e4196704452f62ba4d
Mark Hindess <soft-github@temporalanomaly.com>
2008-03-13 19:33:10 +0000

    Fixing copyright statements.

--- lib/xPL/Timer/cron.pm
+++ lib/xPL/Timer/cron.pm
@@ -97,7 +97,7 @@ Mark Hindess, E<lt>xpl-perl@beanz.uklinux.netE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2006 by Mark Hindess
+Copyright (C) 2006, 2007 by Mark Hindess
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.7 or,
--- t/01-xpl-message.t
+++ t/01-xpl-message.t
@@ -1,6 +1,6 @@
 #!/usr/bin/perl -w
 #
-# Copyright (C) 2007 by Mark Hindess
+# Copyright (C) 2007, 2008 by Mark Hindess
 
 use strict;
 use Test::More tests => 12;
