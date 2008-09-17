1dfc0a6a063136fe9b97cb086673d8e91e83f191
Mark Hindess <soft-github@temporalanomaly.com>
2008-09-17 21:48:02 +0000

    Adding support for KR21 keyfob.

--- lib/xPL/RF/X10Security.pm
+++ lib/xPL/RF/X10Security.pm
@@ -66,67 +66,96 @@ sub parse {
     (
      # See: http://www.wgldesigns.com/protocols/w800rf32_protocol.txt
      0x70 => 'SH624 arm home (min)',
-     0x60 => 'SH624 arm away (min)',
+     #0x60 => 'SH624 arm away (min)',
      0x50 => 'SH624 arm home (max)',
      0x40 => 'SH624 arm away (max)',
      0x41 => 'SH624 disarm',
      0x42 => 'SH624 sec light on',
      0x43 => 'SH624 sec light off',
      0x44 => 'SH624 panic',
-     #0x60 => 'KF574 arm',
-     0x61 => 'KF574 disarm',
-     0x62 => 'KF574 lights on',
-     0x63 => 'KF574 lights off',
+     0x60 => 'arm-away',
+     0x61 => 'disarm',
+     0x62 => 'lights-on',
+     0x63 => 'lights-off',
     );
 
-  if (exists $not_supported_yet{$data}) {
-    warn sprintf "Not supported: %02x %s\n", $data, $not_supported_yet{$data};
-    return [];
-  }
+  my %x10_security =
+    (
+     0x60 => ['arm-away', 'min'],
+     0x61 => 'disarm',
+     0x62 => 'lights-on',
+     0x63 => 'lights-off',
+    );
 
-  my $alert = !($data&0x1);
-  my $tamper = $data&0x2;
-  my $min_delay = $data&0x20;
-  my $low_battery = $data&0x80;
+  my $command;
+  my $tamper;
+  my $min_delay;
+  my $low_battery;
 
   my @res;
-  my %args =
-    (
-     message_type => 'xpl-trig',
-     class => 'security.zone',
-     head => { source => $parent->source, },
-     body => {
-              event => 'alert',
-              zone  => $device,
-              state => $alert ? 'true' : 'false',
-             }
-    );
-  $args{'body'}->{'tamper'} = 'true' if ($tamper);
-  $args{'body'}->{'low-battery'} = 'true' if ($low_battery);
-  $args{'body'}->{'delay'} = $min_delay ? 'min' : 'max';
-  push @res, xPL::Message->new(%args);
-#x10.security
-#{
-#command=alert|normal|motion|light|dark|arm-home|arm-away|disarm|panic|lights-on|lights-off
-#device=<device id>
-#[type=sh624|kr10|ds10|ds90|ms10|ms20|ms90|dm10|sd90|...]
-#[tamper=true|false]
-#[low-battery=true|false]
-#[delay=min|max]
-#}
+  my %args;
+  if (exists $x10_security{$data}) {
+    my $rec = $x10_security{$data};
+    my $min_delay;
+    if (ref $rec) {
+      ($command, $min_delay) = @$rec;
+    } else {
+      $command = $rec;
+    }
+
+    %args = (
+             message_type => 'xpl-trig',
+             class => 'security.basic',
+             head => { source => $parent->source, },
+             body => {
+                      command => $command,
+                      user => $device,
+                     }
+            );
+    $args{'body'}->{'delay'} = $min_delay if (defined $min_delay);
+    push @res, xPL::Message->new(%args);
+
+  } elsif (exists $not_supported_yet{$data}) {
+    warn sprintf "Not supported: %02x %s\n", $data, $not_supported_yet{$data};
+    return [];
+  } else {
+
+    my $alert = !($data&0x1);
+    $command = $alert ? 'alert' : 'normal',
+    $tamper = $data&0x2;
+    $min_delay = $data&0x20 ? 'min' : 'max';
+    $low_battery = $data&0x80;
+
+    %args =
+      (
+       message_type => 'xpl-trig',
+       class => 'security.zone',
+       head => { source => $parent->source, },
+       body => {
+                event => 'alert',
+                zone  => $device,
+                state => $alert ? 'true' : 'false',
+               }
+      );
+    $args{'body'}->{'tamper'} = 'true' if ($tamper);
+    $args{'body'}->{'low-battery'} = 'true' if ($low_battery);
+    $args{'body'}->{'delay'} = $min_delay;
+    push @res, xPL::Message->new(%args);
+  }
+
   %args =
     (
      message_type => 'xpl-trig',
      class => 'x10.security',
      head => { source => $parent->source, },
      body => {
-              command => $alert ? 'alert' : 'normal',
+              command => $command,
               device  => $short_device,
              }
     );
   $args{'body'}->{'tamper'} = 'true' if ($tamper);
   $args{'body'}->{'low-battery'} = 'true' if ($low_battery);
-  $args{'body'}->{'delay'} = $min_delay ? 'min' : 'max';
+  $args{'body'}->{'delay'} = $min_delay if (defined $min_delay);
   push @res, xPL::Message->new(%args);
   return \@res;
 }
@@ -155,7 +184,7 @@ Mark Hindess, E<lt>xpl-perl@beanz.uklinux.netE<gt>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright (C) 2007 by Mark Hindess
+Copyright (C) 2008 by Mark Hindess
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.7 or,
