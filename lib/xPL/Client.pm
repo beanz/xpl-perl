698d6d69b6e66b43996fa8723cbe51af4ace2ff5
Mark Hindess <soft-github@temporalanomaly.com>
2008-11-19 20:44:06 +0000

    Fix instance_id length.  No idea why this was wrong here but not in xPL::Message.  Francois Corthay spotted this.  Thanks!

--- lib/xPL/Client.pm
+++ lib/xPL/Client.pm
@@ -104,9 +104,9 @@ sub new {
   }
 
   exists $p{instance_id} or
-    $p{instance_id} = substr $ENV{XPL_HOSTNAME}||(uname)[1]||'default', 0, 12;
+    $p{instance_id} = substr $ENV{XPL_HOSTNAME}||(uname)[1]||'default', 0, 16;
   $p{instance_id} =~ s/\..*$//; # strip domain if there is one
-  $p{instance_id}=~/^[A-Za-z0-9]{1,12}$/ or
+  $p{instance_id}=~/^[A-Za-z0-9]{1,16}$/ or
     $self->argh('instance_id, '.$p{instance_id}.", is invalid.\n".
       "The default can be overriden by setting the XPL_HOSTNAME environment\n".
       'variable');
