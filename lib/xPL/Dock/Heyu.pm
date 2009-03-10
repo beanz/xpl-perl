b11a2fe03821c7d2a1d153451b30ff6940d2ca18
Mark Hindess <soft-github@temporalanomaly.com>
2009-03-10 22:59:23 +0000

    Refactored xpl-x10 to xPL::Dock::Heyu and created new xpl-heyu client.

--- lib/xPL/Dock/Heyu.pm
+++ lib/xPL/Dock/Heyu.pm
@@ -1,119 +1,114 @@
-#!/usr/bin/perl -w
-
-# $Id$
+package xPL::Dock::Heyu;
 
 =head1 NAME
 
-xpl-x10 - Perl script for an xPL X10 application
+xPL::Dock::Heyu - xPL::Dock plugin for bluetooth proximity reporting
 
 =head1 SYNOPSIS
 
-  xpl-x10 [flags] [options]
-  where valid flags are:
-    -h - show this help text
-    -v - verbose mode
-  and valid options are (default shown in brackets):
-    -i if0 - the interface for xPL messages (first non-loopback or loopback)
-
-  # start the x10 application on first ethernet interface in verbose mode
-  xpl-x10 -i eth0 -v
+  use xPL::Dock qw/Heyu/;
+  my $xpl = xPL::Dock->new();
+  $xpl->main_loop();
 
 =head1 DESCRIPTION
 
-This script is an xPL client that interfaces with an X10 controller.
-At the moment, this script uses heyu (version 2) to communicate with
-the controller.
+This L<xPL::Dock> plugin adds bluetooth proximity reporting.
+
+=head1 METHODS
 
 =cut
 
+use 5.006;
 use strict;
 use warnings;
+
 use English qw/-no_match_vars/;
 use FileHandle;
 use IO::Pipe;
 use POSIX ":sys_wait_h";
-use Getopt::Long;
-use Pod::Usage;
-use xPL::Client;
 use xPL::Queue;
-$|=1; # autoflush helps debugging
-my $path = ($0=~m!^(.*/)!) ? $1 : '';
+use xPL::Dock::Plug;
 
-my %args = ( vendor_id => 'bnz', device_id => 'x10heyu', );
-my %opt = ();
-my $verbose;
-my $interface;
-my $help;
-my $man;
-GetOptions('verbose+' => \$verbose,
-           'interface=s' => \$interface,
-           'define=s' => \%opt,
-           'help|?|h' => \$help,
-           'man' => \$man,
-          ) or pod2usage(2);
-pod2usage(1) if ($help);
-pod2usage(-exitstatus => 0, -verbose => 2) if ($man);
+our @ISA = qw(xPL::Dock::Plug);
+our %EXPORT_TAGS = ( 'all' => [ qw() ] );
+our @EXPORT_OK = ( @{ $EXPORT_TAGS{'all'} } );
+our @EXPORT = qw();
+our $VERSION = qw/$Revision$/[1];
 
-$args{'interface'} = $interface if ($interface);
-$args{'verbose'} = $verbose if ($verbose);
-
-# Create an xPL Client object
-my $xpl = xPL::Client->new(%args, %opt) or die "Failed to create xPL::Client\n";
-
-# Add a callback to receive all incoming xPL messages
-$xpl->add_xpl_callback(id => 'x10', callback => \&xpl_in,
-                       filter => {
-                                  message_type => 'xpl-cmnd',
-                                  class => 'x10',
-                                  class_type => 'basic',
-                                 });
-
-my $buf = q{};
-my $helperbuf = q{};
-my $helperseq = 0;
-my $waiting;
-my $queue = xPL::Queue->new();
-my %unit = ();
-
-my $fh = new FileHandle;
-$fh->open('heyu monitor|') || die "heyu: $ERRNO\n";
-$xpl->add_input(handle => $fh, callback => \&heyu_monitor);
-
-my $rh = IO::Pipe->new;
-my $wh = IO::Pipe->new;
-my $pid = fork;
-if ($pid) {
-
-  sub sig {
-    waitpid(-1,WNOHANG);
-    die "Caught signal\n";
-  }
-  $SIG{CHLD} = \&sig;
-  $SIG{PIPE} = \&sig;
-
-  # parent
-  $rh->reader();
-  $wh->writer();
-  $wh->autoflush(1);
-  $xpl->add_input(handle => $rh, callback => \&heyu_helper_read);
-} elsif (defined $pid) {
-  # child
-  $rh->writer();
-  $rh->autoflush(1);
-  $wh->reader();
-  my $wfd = $rh->fileno;
-  my $rfd = $wh->fileno;
-  open(STDIN,"<&$rfd") or die "dup of stdin failed: $!";
-  open(STDOUT,">&=$wfd") or die "dup of stdout failed: $!";
-  open(STDERR,"+>&$wfd") or die "dup of stderr failed: $!";
-  exec($path.'xpl-heyu-helper',  @ARGV) or
-    die "Failed to exec ${path}xpl-heyu-helper: $!\n";
-} else {
-  die "Fork failed: $!\n";
+sub getopts {
+  my $self = shift;
+  return (
+          'heyu-verbose|heyuverbose+' => \$self->{_verbose},
+         );
 }
 
-# Run the main loop
-$xpl->main_loop();
+=head2 C<init(%params)>
+
+=cut
+
+sub init {
+  my $self = shift;
+  my $xpl = shift;
+  my %p = @_;
+
+  $self->SUPER::init($xpl, @_);
+
+  $self->{_buffer} = q{};
+  $self->{_helper_buffer} = q{};
+  $self->{_helper_seq} = 0;
+  $self->{_waiting} = undef;
+  $self->{_q} = xPL::Queue->new();
+  $self->{_unit} = {};
+
+  # Add a callback to receive all incoming xPL messages
+  $xpl->add_xpl_callback(id => 'x10', callback => sub { $self->xpl_in(@_) },
+                         filter => {
+                                    message_type => 'xpl-cmnd',
+                                    class => 'x10',
+                                    class_type => 'basic',
+                                   });
+
+  my $fh = $self->{_monitor_fh} =
+    IO::Pipe->reader(qw/heyu monitor/) or
+        $self->argh("'heyu monitor|' failed: $! $@\n");
+  $xpl->add_input(handle => $fh, callback => sub { $self->heyu_monitor(@_) });
+
+  my $rh = $self->{_helper_rh} = IO::Pipe->new;
+  my $wh = $self->{_helper_wh} = IO::Pipe->new;
+  my $pid = fork;
+  if ($pid) {
+
+    sub sig {
+      waitpid(-1,WNOHANG);
+      die "Caught signal\n";
+    }
+    $SIG{CHLD} = \&sig;
+    $SIG{PIPE} = \&sig;
+
+    # parent
+    $rh->reader();
+    $wh->writer();
+    $wh->autoflush(1);
+    $xpl->add_input(handle => $rh,
+                    callback => sub { $self->heyu_helper_read(@_) });
+  } elsif (defined $pid) {
+    # child
+    $rh->writer();
+    $rh->autoflush(1);
+    $wh->reader();
+    my $wfd = $rh->fileno;
+    my $rfd = $wh->fileno;
+    open(STDIN,"<&$rfd") or die "dup of stdin failed: $!";
+    open(STDOUT,">&=$wfd") or die "dup of stdout failed: $!";
+    open(STDERR,"+>&$wfd") or die "dup of stderr failed: $!";
+    my $path = ($0=~m!^(.*/)!) ? $1 : '';
+    exec($path.'xpl-heyu-helper',  @ARGV) or
+      $self->argh("Failed to exec ${path}xpl-heyu-helper: $!\n");
+  } else {
+    $self->argh("Fork for xpl-heyu-helper failed: $!\n");
+  }
+  return $self;
+}
 
 =head2 C<xpl_in(%xpl_callback_parameters)>
 
@@ -124,6 +119,7 @@ extended.
 =cut
 
 sub xpl_in {
+  my $self = shift;
   my %p = @_;
   my $msg = $p{message};
   my $peeraddr = $p{peeraddr};
@@ -152,7 +148,7 @@ sub xpl_in {
     $data1 = sprintf '%02x', $data1;
     $data2 = sprintf '%02x', $data2;
     foreach my $device (@devices) {
-      heyu_helper_queue($heyu_command, $data1, $device, $data2);
+      $self->heyu_helper_queue($heyu_command, $data1, $device, $data2);
       my %args = (
                   message_type => 'xpl-trig',
                   class => 'x10.confirm',
@@ -160,13 +156,13 @@ sub xpl_in {
                             device => device_heyu_to_xpl($device),
                             data1 => $msg->data1, data2 => $msg->data2 },
                  );
-      $xpl->send(%args);
+      $self->xpl->send(%args);
     }
     return 1;
   }
 
   foreach my $device (@devices) {
-    heyu_helper_queue($heyu_command, $device, @args);
+    $self->heyu_helper_queue($heyu_command, $device, @args);
   }
   return 1;
 }
@@ -180,8 +176,10 @@ messages.
 =cut
 
 sub heyu_monitor {
-  my $bytes = $fh->sysread($buf, 512, length($buf));
-  while ($buf =~ s/^(.*?)\n//) {
+  my $self = shift;
+  my $bytes = $self->{_monitor_fh}->sysread($self->{_buffer}, 512,
+                                            length($self->{_buffer}));
+  while ($self->{_buffer} =~ s/^(.*?)\n//) {
     $_ = $LAST_PAREN_MATCH;
     my $class = 'x10.basic';
     $class = 'x10.confirm' if (/sndc/);
@@ -190,18 +188,18 @@ sub heyu_monitor {
       my $f = lc($1);
       my $h = lc($2);
       my $level = $4;
-      my $u = join ',', sort { $a <=> $b } @{$unit{$h}|| ['0']};
+      my $u = join ',', sort { $a <=> $b } @{$self->{_unit}->{$h}|| ['0']};
       send_xpl($class, $h.$u, $f, $level);
-      delete $unit{$h};
+      delete $self->{_unit}->{$h};
     } elsif (m!function\s+xPreset\s+:\s+housecode\s+(\w)\s+unit\s+(\d+)\s+level\s+(\d+)!) {
       my $f = 'xfunc';
       my $h = lc($1);
       my $u = $2;
       my $level = [ 49, $3 ];
       send_xpl($class, $h.$u, $f, $level);
-      delete $unit{$h}; # TODO: should we do this? need to check spec
+      delete $self->{_unit}->{$h}; # TODO: should we do this? need to check spec
     } elsif (m!address\s+unit\s+(\S+)\s+:\s+housecode\s+(\w+)!) {
-      push @{$unit{lc($2)}}, $1;
+      push @{$self->{_unit}->{lc($2)}}, $1;
     } else {
       print STDERR "monitor reported unsupported line:\n  $_\n";
     }
@@ -218,21 +216,23 @@ command.  It is responsible for reading the results of heyu commands.
 =cut
 
 sub heyu_helper_read {
-  my $bytes = $rh->sysread($helperbuf, 512, length($helperbuf));
-  while ($helperbuf =~ s/^(.*?)\n//) {
+  my $self = shift;
+  my $bytes = $self->{_helper_rh}->sysread($self->{_helper_buffer}, 512,
+                                           length $self->{_helper_buffer});
+  while ($self->{_helper_buffer} =~ s/^(.*?)\n//) {
     $_ = $LAST_PAREN_MATCH;
     my ($recvseq, $rc, $err) = split /\000/, $_, 3;
     unless ($recvseq =~ /^[0-9a-f]{8}$/) {
       print STDERR "Helper wrote: $_\n";
       next;
     }
-    if ($recvseq eq $waiting && $rc == 0) {
-      print STDERR "Acknowledged $waiting\n";
+    if ($recvseq eq $self->{_waiting} && $rc == 0) {
+      print STDERR "Acknowledged ".$self->{_waiting}."\n";
     } else {
       print STDERR "Received $recvseq: $rc ", $err||"", "\n";
     }
-    undef $waiting;
-    heyu_helper_write();
+    undef $self->{_waiting};
+    $self->heyu_helper_write();
   }
   return 1;
 }
@@ -244,13 +244,14 @@ This method is used to queue commands to the heyu helper.
 =cut
 
 sub heyu_helper_queue {
-  my $seq_str = sprintf "%08x", $helperseq++;
+  my $self = shift;
+  my $seq_str = sprintf "%08x", $self->{_helper_seq}++;
   my $msg = join chr(0), $seq_str, @_;
   $msg .= "\n";
-  $queue->enqueue([$seq_str, $msg]);
+  $self->{_q}->enqueue([$seq_str, $msg]);
   $msg =~ s/\0/ /g;
-  print STDERR "queued: $msg" if ($verbose);
-  return heyu_helper_write() if (!defined $waiting);
+  print STDERR "queued: $msg" if ($self->verbose);
+  return $self->heyu_helper_write() if (!defined $self->{_waiting});
   return $seq_str;
 }
 
@@ -261,13 +262,14 @@ This method is used to send commands to the heyu helper.
 =cut
 
 sub heyu_helper_write {
-  my $item = $queue->dequeue;
+  my $self = shift;
+  my $item = $self->{_q}->dequeue;
   return unless (defined $item);
   my ($seq_str, $msg) = @$item;
-  $wh->syswrite($msg);
+  $self->{_helper_wh}->syswrite($msg);
   $msg =~ s/\0/ /g;
-  print STDERR "sent: $msg" if ($verbose);
-  $waiting = $seq_str;
+  print STDERR "sent: $msg" if ($self->verbose);
+  $self->{_waiting} = $seq_str;
   return $seq_str;
 }
 
@@ -279,6 +281,7 @@ result of messages from "heyu monitor".
 =cut
 
 sub send_xpl {
+  my $self = shift;
   my $class = shift;
   my $device = shift;
   my $command = shift;
@@ -301,12 +304,12 @@ sub send_xpl {
   } elsif ($level) {
     $args{body}->{level} = level_heyu_to_xpl($level);
   }
-  if ($verbose) {
+  if ($self->verbose) {
     print STDERR
       "Sending $class $device $command", ($level ? " ".$level : ""), "\n";
   }
 
-  $xpl->send(%args);
+  $self->xpl->send(%args);
 }
 
 # convert level from 0-100 range for xPL to 0-22 range for heyu
@@ -374,12 +377,16 @@ sub command_heyu_to_xpl {
          }->{$command};
 }
 
-# send a 'hbeat.end' message on exit
-END { defined $xpl && $xpl->send_hbeat_end(); }
+1;
+__END__
+
+=head1 EXPORT
+
+None by default.
 
 =head1 SEE ALSO
 
-xPL::Client(3), xPL::Listener(3), heyu(1)
+xPL::Dock(3), heyu(1)
 
 Project website: http://www.xpl-perl.org.uk/
 
@@ -389,7 +396,7 @@ Mark Hindess, E<lt>soft-xpl-perl@temporalanomaly.comE<gt>
 
 =head1 COPYRIGHT
 
-Copyright (C) 2005, 2008 by Mark Hindess
+Copyright (C) 2005, 2009 by Mark Hindess
 
 This library is free software; you can redistribute it and/or modify
 it under the same terms as Perl itself, either Perl version 5.8.7 or,
