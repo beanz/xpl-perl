9605b3b900906db9c2f7519edb9c3486625e579b
Mark Hindess <soft-github@temporalanomaly.com>
2009-06-09 20:06:23 +0000

    Remove duplicate POD.

--- lib/xPL/Dock/Owfs.pm
+++ lib/xPL/Dock/Owfs.pm
@@ -294,33 +294,6 @@ Mark Hindess, E<lt>soft-xpl-perl@temporalanomaly.comE<gt>
 
 =head1 COPYRIGHT
 
-Copyright (C) 2006, 2008 by Mark Hindess
-
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself, either Perl version 5.8.7 or,
-at your option, any later version of Perl 5 you may have available.
-
-=cut
-
-1;
-__END__
-
-=head1 EXPORT
-
-None by default.
-
-=head1 SEE ALSO
-
-xPL::Dock(3), heyu(1)
-
-Project website: http://www.xpl-perl.org.uk/
-
-=head1 AUTHOR
-
-Mark Hindess, E<lt>soft-xpl-perl@temporalanomaly.comE<gt>
-
-=head1 COPYRIGHT
-
 Copyright (C) 2005, 2009 by Mark Hindess
 
 This library is free software; you can redistribute it and/or modify
