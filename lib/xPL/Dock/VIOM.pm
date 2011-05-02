e608438ceabc574ff989189721086ca0929d092e
Mark Hindess <soft-github@temporalanomaly.com>
2011-05-02 21:56:32 +0100

    Remove unused code.

--- lib/xPL/Dock/VIOM.pm
+++ lib/xPL/Dock/VIOM.pm
@@ -186,28 +186,6 @@ sub process_line {
   return 1;
 }
 
-=head2 C<send_xpl( $device, $level )>
-
-This functions is used to send out sensor.basic xpl-trig messages as a
-result of changes to the VIOM inputs.
-
-=cut
-
-sub send_xpl {
-  my $self = shift;
-  my $device = shift;
-  my $level = shift;
-  my $xpl = $self->xpl;
-  my %args =
-    (
-     message_type => 'xpl-trig',
-     schema => 'sensor.basic',
-     body => [ device => $device, type => 'input', current => $level ],
-    );
-  $self->info('Sending ', $device, ' ', $level, "\n");
-  return $xpl->send(%args);
-}
-
 =head2 C<current_state( $type, $num )>
 
 Returns the current state of the input or output.
